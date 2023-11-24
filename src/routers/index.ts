0db204dfaef4ec90e82ff99999ebff3bdd634008
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-11-25 00:04:59 +0600

    added index route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,42 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { sendResponse } from "@/helpers/response";
+import axios from "axios";
 const router = express.Router();
 
-// test
-router.get("/", (req, res) => {
-  res.send({ message: "hello mongoexp" });
+// test index route
+router.get("/", async (req: Request, res: Response) => {
+  const {
+    data: { ip },
+  } = await axios.get("https://api.ipify.org/?format=json");
+  const { data: ipInfo } = await axios.get(`http://ip-api.com/json/${ip}`);
+
+  try {
+    sendResponse({
+      res,
+      response: {
+        success: true,
+        data: {
+          country: ipInfo?.country,
+          regionName: ipInfo?.regionName,
+          city: ipInfo?.city,
+          zip: ipInfo?.zip,
+        },
+        message: "Hello from mongoexp",
+      },
+    });
+  } catch (error: any) {
+    sendResponse({
+      res,
+      response: {
+        success: false,
+        message: "Internal server error",
+        error: {
+          code: 500,
+          description: error.stack || "unknown error",
+        },
+      },
+    });
+  }
 });
 
 export default router;
