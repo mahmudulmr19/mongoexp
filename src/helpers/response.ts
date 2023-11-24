24fa945722a82b330758f1d1d3e2e9ac20fa2fcd
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-11-24 23:05:50 +0600

    added response send helper

---
+++ src/helpers/response.ts
@@ -0,0 +1,47 @@
+import { Response } from "express";
+
+// Define response types
+interface BaseResponse {
+  success: boolean;
+  message: string;
+}
+
+interface SuccessResponse<T> extends BaseResponse {
+  success: true;
+  data: T;
+}
+
+interface ErrorResponse extends BaseResponse {
+  success: false;
+  error: {
+    code: number;
+    description: string;
+  };
+}
+
+// Combined response type using discriminated unions
+type DynamicResponse<T> = SuccessResponse<T> | ErrorResponse;
+
+// Props interface for the function
+interface ResponseHandlerProps<T> {
+  res: Response;
+  response: DynamicResponse<T>;
+}
+
+// Function to handle dynamic responses
+export function sendResponse<T>({
+  res,
+  response,
+}: ResponseHandlerProps<T>): void {
+  const { success, message } = response;
+
+  if (success) {
+    const successResponse = response as SuccessResponse<T>;
+    const { data } = successResponse;
+    res.status(200).json({ success, message, data });
+  } else {
+    const errorResponse = response as ErrorResponse;
+    const { error } = errorResponse;
+    res.status(error.code).json({ success, message, error });
+  }
+}
