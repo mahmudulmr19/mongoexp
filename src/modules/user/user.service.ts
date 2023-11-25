d9094778e067a5f407d5cfb3d3e38bb4fcefce28
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-11-25 22:39:53 +0600

    updated get all users service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -12,7 +12,17 @@ const createUserIntoDB = async (usersData: User) => {
 };
 
 const getAllUserFromDB = async () => {
-  const users = await UserModel.find();
+  const users = await UserModel.find(
+    {},
+    {
+      username: 1,
+      fullName: 1,
+      age: 1,
+      email: 1,
+      address: 1,
+      _id: 0,
+    }
+  );
   return users;
 };
 
